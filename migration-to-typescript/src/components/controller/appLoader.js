fbf5fd845398839a8fec018909c6bd52394b6e4e
Kirill Molotkov <balbes_goda@mail.ru>
2022-12-12 12:14:09 +0300

    feat: added api Key and initialized npm

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd1891ce7ad5f42f9a39ff887babdc0a8', // получите свой ключ https://newsapi.org/
         });
     }
 }
