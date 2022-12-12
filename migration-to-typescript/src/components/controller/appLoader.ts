e59283683a367203cea3d9456fc1ed1c3896bc1d
Kirill Molotkov <balbes_goda@mail.ru>
2022-12-13 01:24:37 +0300

    fix: link in appLoader.ts for deploy

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'd1891ce7ad5f42f9a39ff887babdc0a8', // получите свой ключ https://newsapi.org/
         });
     }
